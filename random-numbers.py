e4f6771e39eba4934d24f38f5ea2fa6016201c13
oluwaseun007 <preciousopal.od@gmail.com>
2022-07-08 23:04:28 +0100

    print random number from a range

---
+++ random-numbers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+# Random Number
+
+    Python has a built-in module called 'random' that
+    can be used to make random numbers.
+"""
+
+import random
+
+print(random.randrange(1, 10))
+
+# This program prints a randim number between 1 and 9.
+# The range can be increased.
+
+print(random.randrange(65, 250))
